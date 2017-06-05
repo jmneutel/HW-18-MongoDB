0fac29ebdff4eb03107024beabe5660ee970a6fc
Jared Neutel <Jared@Macintosh-2.local>
2017-06-04 22:57:47 -0700

    changed server.js

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ mongoose.Promise = Promise;
 // Initialize Express
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -152,6 +154,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(port, function() {
+  console.log("App running on "+port+"!");
 });
